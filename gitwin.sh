9af7c2b3abdcbd5c4dc0f1e6235c61985c6d04db
leaphe <13918825181@qq.com>
2019-10-08 21:41:39 +0800

    Davis

---
+++ gitwin.sh
@@ -0,0 +1,39 @@
+﻿#!bin/sh
+
+
+DATE=`date`
+echo "Date is $DATE"
+
+startTime=`date +%Y%m%d-%H:%M`
+ 
+startTime_s=`date +%s`
+
+echo "Date is $startTime"
+
+
+
+pwd
+git --version
+git config user.name 'leaphe'
+git config user.email '13918825181@qq.com'
+git add .
+echo '正在提交本地库'
+git commit -m 'Davis'
+echo '正在提交远程库'
+git push origin master
+echo '执行成功'
+
+
+ 
+endTime=`date +%Y%m%d-%H:%M` 
+ 
+endTime_s=`date +%s`
+
+
+sumTime=$[ $endTime_s - $startTime_s ]
+ 
+echo "$startTime ---> $endTime" "Totl:$sumTime Second" 
+
+
+
+ 
